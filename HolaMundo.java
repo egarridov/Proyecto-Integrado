6e708e65fd9f8232a315b025cbfb548355cac751
egarridov <57188916+egarridov@users.noreply.github.com>
2019-11-20 16:11:28 +0100

    Colores definidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,2 +1,9 @@
-System.out.println("Hola mundo")
-System.out.println("Adios")
+public class HolaMundo {
+	public static void main (string[] args){
+	 string naranja = "\033[33m";
+	 string azul = "\033[34m";
+
+	 System.out.println(naranja + "¡Hola mundo!");
+	 System.out.println(azul + "Aquí estamos haciendo pruebas con Git.");
+	}
+}
